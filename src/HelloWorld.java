16d3ba71da715353e97a5134c7c79cc8cc0f9985
Owner <jamesbond00r@gmail>
2020-01-11 20:10:10 -0600

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+        int numberOfDays;
+        numberOfDays = 7;
+
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println(1);
+
+    }
+
+}
