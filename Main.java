c05a83a9def77c790dbe1c23fef1160d06071da8
Rob <37629428+jamesbond00r@users.noreply.github.com>
2020-01-14 20:57:27 -0600

    Playing with Longs, Bytes and shorts

--- Main.java
+++ Main.java
@@ -12,5 +12,21 @@ public class Main {
         System.out.println(myMinValue + myMaxValue);
         System.out.println("Busted max = " + (myMaxValue + 2));
         System.out.println("Busted Min = " + (myMinValue -1));
+
+             byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Max Value =  " + myMaxByteValue);
+        System.out.println("Byte Min Value = " + myMinByteValue);
+
+        short myMinshortValue = Short.MIN_VALUE;
+        short myMaxshortValue = Short.MAX_VALUE;
+        System.out.println("short Max Value =  " + myMaxshortValue);
+        System.out.println("short Min Value = " + myMinshortValue);
+
+        long myMinlongValue = Long.MIN_VALUE;
+        long myMaxlongValue = Long.MAX_VALUE;
+        System.out.println("long Max Value =  " + myMaxlongValue);
+        System.out.println("long Min Value = " + myMinlongValue);
+    long myLongValue = 100L;
     }
 }
